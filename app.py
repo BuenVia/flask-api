ce06ce35546d9e2619415a40cd4fad697c016ecf
Matthew Clifford <matthewclifford@hotmail.co.uk>
2023-11-23 15:32:43 +0000

    change to codebase

--- app.py
+++ app.py
@@ -1,22 +1,57 @@
-from flask import Flask
+import os
+
+from flask import Flask, jsonify
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+
+import models
+
+from db import db
+from resources.user import blp as UserBlueprint
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 
-app = Flask(__name__)
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['API_TITLE'] = "Stores REST API"
-app.config['API_VERSION'] = "v1"
-app.config['OPENAPI_VERSION'] = "3.0.3"
-app.config['OPENAPI_URL_PREFIX'] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+    app.config['API_TITLE'] = "Stores REST API"
+    app.config['API_VERSION'] = "v1"
+    app.config['OPENAPI_VERSION'] = "3.0.3"
+    app.config['OPENAPI_URL_PREFIX'] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    api = Api(app)
 
-api = Api(app)
+    app.config["JWT_SECRET_KEY"] = "matt"
+    jwt = JWTManager(app)
 
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return jsonify({"message": "The token has expired", "error": "token expired"}), 401
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({"message": "Signature verification failed.", "error": "invalid token"}), 401
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({"description": "Request does not contain an access token", "error": "authorization required"}), 401
+
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(UserBlueprint)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
